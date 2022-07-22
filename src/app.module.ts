9d6da2c1d163f2a84a7c5f6d805a0072b9b88150
Daniel <danielrs9504@gmail.com>
2022-07-22 13:58:44 -0500

    1. Add typeorm 2. Add Note Entity and use it on his module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [NotesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'notesdb',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      retryDelay: 3000,
+      retryAttempts: 10,
+    }),
+    NotesModule,
+  ],
   controllers: [],
   providers: [],
 })
