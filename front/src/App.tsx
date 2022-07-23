1dbe9167db5deda35aca004448f85d61f6973f6b
Daniel <danielrs9504@gmail.com>
2022-07-23 06:07:33 -0500

    1. Add axios for requests 2. Change server port from 3000 to 5000 3. Add basic INSERT note from front

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,45 @@
-import { CloseOutlined, SaveOutlined } from '@ant-design/icons';
-import { Button, Layout, PageHeader } from 'antd';
+import { CheckOutlined, CloseOutlined, SaveOutlined } from '@ant-design/icons';
+import { Button, Form, Layout, notification, PageHeader } from 'antd';
 import Modal from 'antd/lib/modal/Modal';
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import FormNote from './components/FormNote';
-import NotesList from './components/NotesList';
+import NotesList, { Note } from './components/NotesList';
 
-function App() {
+const App = () => {
+  // * ========== Variables ==========
+  const [formNote] = Form.useForm();
+
   // * ========== States ==========
+  const [notes, setNotes] = useState([]);
   const [showModal, setShowModal] = useState<boolean>(false);
 
   // * ========== Handlers ==========
   const buttonOnClick = () => setShowModal(true)
+
+  const submitNote = async (note: Partial<Note>) => {
+    console.log(note)
+    await axios.post('/api/notes', note)
+      .then(() => notification.open({
+        message: 'Success',
+        description: 'Note creation',
+        icon: <CheckOutlined />
+      }))
+      .then(() => setShowModal(false))
+  }
+
+  const onOkModal = () => {
+    formNote.validateFields()
+    .then(() => formNote.submit())
+    .catch(err => { throw err })
+  }
+
+  // * ========== UseEffects ==========
+  useEffect(() => {
+    axios.get('/api/notes')
+    .then(response => setNotes(response.data))
+    .catch(err => { throw err })
+  }, [])
 
   return (
     <Layout>
@@ -18,7 +47,9 @@ function App() {
         title="My notes"
         subTitle={<Button onClick={buttonOnClick}>Add note</Button>}
       />
-      <NotesList />
+      <NotesList
+        notes={notes}
+      />
       <Modal
         title="Create note"
         centered
@@ -27,11 +58,16 @@ function App() {
         cancelButtonProps={{ icon: <CloseOutlined /> }}
         okText="Save"
         okButtonProps={{ icon: <SaveOutlined /> }}
+        onOk={onOkModal}
       >
-        <FormNote />
+        <FormNote
+          form={formNote}
+          initialValues={{}}
+          onSubmit={submitNote}
+        />
       </Modal>
     </Layout>
-  );
+  )
 }
 
-export default App;
+export default App
--- front/src/components/FormNote.tsx
+++ front/src/components/FormNote.tsx
@@ -1,28 +1,43 @@
-import { Col, Form, Input, Row } from 'antd'
+import { Col, Form, FormInstance, Input, Row } from 'antd'
 import TextArea from 'antd/lib/input/TextArea'
+import { FC, useEffect } from 'react';
 import { Note } from './NotesList'
 
-const FormNote = () => {
+type FormNoteProps = {
+  form: FormInstance;
+  initialValues?: Partial<Note>;
+  onSubmit: (note: Partial<Note>) => void;
+}
+
+const FormNote: FC<FormNoteProps> = ({
+  form,
+  initialValues,
+  onSubmit,
+}) => {
+  useEffect(() => {
+    form?.resetFields();
+  }, [initialValues]);
+
   return (
     <Form<Note>
-      // onFinish={onSubmit}
-      // initialValues={initialValues}
-      // form={form}
+      onFinish={onSubmit}
+      initialValues={initialValues}
+      form={form}
       labelCol={{ span: 4 }}
       wrapperCol={{ span: 18 }}
-      // layout="horizontal"
       >
       <Row justify="center">
         <Form.Item hidden name="id" />
+        <Form.Item hidden name="archieved" />
 
         <Col flex="1 1 300px">
-          <Form.Item label="Title">
+          <Form.Item label="Title" name="title" required>
             <Input />
           </Form.Item>
         </Col>
 
         <Col flex="1 1 300px">
-          <Form.Item label="Content">
+          <Form.Item label="Content" name="content" required>
             <TextArea />
           </Form.Item>
         </Col>
